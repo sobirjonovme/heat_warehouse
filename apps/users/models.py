0f72f7536a7f240965895f75374fc879e04fdc3e
Asadbek <huntsman6004@gmail.com>
2024-01-12 10:01:58 +0500

    initial commit

---
+++ apps/users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+class UserRoles(models.TextChoices):
+    ADMIN = "admin", _("Admin")
+    # for warehouse
+    MAIN_STOCKMAN = "main_stockman", _("Main Stockman")
+    STOCKMAN = "stockman", _("Stockman")
+    WATCHMAN = "watchman", _("Watchman")
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=255, verbose_name=_("Last Name"))
+    role = models.CharField(
+        max_length=20,
+        choices=UserRoles.choices,
+        default=UserRoles.ADMIN,
+    )
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.username
