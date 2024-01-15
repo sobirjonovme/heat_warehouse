862a7b2bb4be52281a862c0b4bb72b8a06553f4e
Asadbek <huntsman6004@gmail.com>
2024-01-15 23:26:46 +0500

    update: User Admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from unfold import admin as unfold_admin
+from unfold.decorators import display
 from unfold.forms import (AdminPasswordChangeForm, UserChangeForm,
                           UserCreationForm)
 
@@ -14,7 +15,7 @@ class UserAdmin(BaseUserAdmin, unfold_admin.ModelAdmin):
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
 
-    list_display = ("id", "username", "first_name", "last_name", "role")
+    list_display = ("id", "username", "first_name", "last_name", "custom_role")
     list_display_links = ("id", "username", "first_name", "last_name")
     search_fields = ("id", "username", "first_name", "last_name")
     list_filter = ("role", "is_active")
@@ -46,3 +47,7 @@ class UserAdmin(BaseUserAdmin, unfold_admin.ModelAdmin):
             },
         ),
     )
+
+    @display(description=_("Role"), ordering="role", label=True)
+    def custom_role(self, obj):
+        return obj.get_role_display()
