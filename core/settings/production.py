7e7e49cba112fd618ec6fef5495b49187c5edd62
Asadbek <huntsman6004@gmail.com>
2024-01-21 13:50:33 +0500

    update: CSRF settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -15,7 +15,14 @@ USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["https://example.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "https://localhost",
+    "http://127.0.0.1",
+    "https://127.0.0.1",
+    "http://heatwarehouse.pythonanywhere.com/",
+    "https://heatwarehouse.pythonanywhere.com/",
+]
 
 ###################################################################
 # CORS
