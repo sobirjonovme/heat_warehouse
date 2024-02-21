4f5a985b3521c5efb298c6bdffe1b85b93980811
Asadbek <huntsman6004@gmail.com>
2024-02-21 11:11:39 +0500

    update: some changes in serializes

--- apps/orders/api_endpoints/order_action/OrderCreate/serializers.py
+++ apps/orders/api_endpoints/order_action/OrderCreate/serializers.py
@@ -24,6 +24,7 @@ class OrderCreateSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "status",
+            "warehouse",
             "items",
         )
         extra_kwargs = {
--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .api_endpoints import warehouse_product
+from .api_endpoints import product, warehouse_product
 
 app_name = "stores"
 
 urlpatterns = [
+    # product
+    path("products/list/", product.ProductListAPIView.as_view(), name="products-list"),
+    path("products/create/", product.ProductCreateAPIView.as_view(), name="product-create"),
+    # warehouse product
     path(
         "warehouse-products/",
         warehouse_product.WarehouseProductListAPIView.as_view(),
