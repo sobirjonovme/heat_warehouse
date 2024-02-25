adaca06bc2de6b9408baac69127c4bbc84f5de0a
Asadbek <huntsman6004@gmail.com>
2024-02-25 21:17:03 +0500

    feat: label field for ProductList and search for WarehouseProductList

--- apps/stores/api_endpoints/warehouse_product/ProductList/views.py
+++ apps/stores/api_endpoints/warehouse_product/ProductList/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,10 +12,14 @@ class WarehouseProductListAPIView(ListAPIView):
     serializer_class = WarehouseProductSerializer
     permission_classes = (IsAuthenticated,)
 
+    filter_backends = (SearchFilter,)
+    search_fields = ("product__name",)
+
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter(warehouse__stockman=self.request.user)
         queryset = queryset.select_related("warehouse", "product", "product__unit")
+        queryset = queryset.order_by("product__name")
         return queryset
 
 
