2255fd8973a1d3d315d9be2204b5918c6b3cb105
Asadbek <huntsman6004@gmail.com>
2024-02-27 16:47:08 +0500

    fix: Order List total money

--- apps/orders/api_endpoints/order/OrderList/serializers.py
+++ apps/orders/api_endpoints/order/OrderList/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from apps.orders.choices import OrderStatus
 from apps.orders.models import Order
 from apps.orders.serializers import UserShortSerializer
 from apps.stores.serializers import WarehouseShortSerializer
@@ -7,8 +8,8 @@ from apps.stores.serializers import WarehouseShortSerializer
 
 class OrderListSerializer(serializers.ModelSerializer):
     # total_money = serializers.DecimalField(max_digits=13, decimal_places=2, read_only=True)
-    warehouse = WarehouseShortSerializer(read_only=True)
     total_money = serializers.SerializerMethodField()
+    warehouse = WarehouseShortSerializer(read_only=True)
     ordered_by = UserShortSerializer(read_only=True)
     main_stockman = UserShortSerializer(read_only=True)
     supplier = UserShortSerializer(read_only=True)
@@ -30,7 +31,7 @@ class OrderListSerializer(serializers.ModelSerializer):
 
     def get_total_money(self, obj):
         total_money = 0
-        if obj.status == "FINAL_CHECKED":
+        if obj.status == OrderStatus.FINAL_CHECKED:
             for item in obj.items.all():
                 total_money += item.cash_amount + item.card_amount + item.debt_amount
 
