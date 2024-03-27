1dc2b85204ba469cb4e84cf034bae8fabcf7e977
Asadbek <huntsman6004@gmail.com>
2024-03-27 22:41:50 +0500

    update: sync order items management command disabled

--- apps/orders/management/commands/sync_order_items.py
+++ apps/orders/management/commands/sync_order_items.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
 
-from apps.orders.choices import OrderStatus
-from apps.orders.models import Order
+# from apps.orders.choices import OrderStatus
+# from apps.orders.models import Order
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        orders = Order.objects.filter(status=OrderStatus.FINAL_CHECKED)
-
-        for order in orders:
-            order.store_items_to_warehouse()
-            self.stdout.write(self.style.NOTICE(f"Order {order.id} items stored to warehouse"))
+        # orders = Order.objects.filter(status=OrderStatus.FINAL_CHECKED)
+        #
+        # for order in orders:
+        #     order.store_items_to_warehouse()
+        #     self.stdout.write(self.style.NOTICE(f"Order {order.id} items stored to warehouse"))
 
         self.stdout.write(self.style.SUCCESS("Finished"))
