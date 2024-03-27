23029aeaa433496cb2c754422b0c849d768d3565
Asadbek <huntsman6004@gmail.com>
2024-03-27 21:42:15 +0500

    feat: management command in orders app to sync order items with warehouse products

---
+++ apps/orders/management/commands/sync_order_items.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from apps.orders.models import Order
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        orders = Order.objects.all()
+
+        for order in orders:
+            order.store_items_to_warehouse()
+            self.stdout.write(self.style.NOTICE(f"Order {order.id} items stored to warehouse"))
+
+        self.stdout.write(self.style.SUCCESS("Finished"))
