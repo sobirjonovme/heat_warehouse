de916bcca436e8d0522e8314e945b4151b6fd8e6
Asadbek <huntsman6004@gmail.com>
2024-03-28 11:58:11 +0500

    update: order telegram notification

--- apps/orders/services/order_notification.py
+++ apps/orders/services/order_notification.py
@@ -1,24 +1,38 @@
 from django.template import loader
-from django.utils import timezone
+from django.utils import dateformat, timezone
+from django.utils.translation import activate
 
 from apps.common.models import TelegramNotification
+from apps.common.services.common import remove_exponent_from_decimal
 from apps.common.services.telegram import send_telegram_message
 
 __all__ = ["send_order_notification"]
 
 
 def send_order_notification(order):
+    activate("uz")
     tg_notification = TelegramNotification.get_solo()
 
     if tg_notification.is_enabled:
-        order_items = order.items.all().select_related("product", "product__unit")
+        order_items = []
+        items_qs = order.items.all().select_related("product", "product__unit")
+        for item in items_qs:
+            order_items.append(
+                {
+                    "product": item.product.name,
+                    "needed_amount": str(remove_exponent_from_decimal(item.needed_amount)),
+                    "unit": item.product.unit.short_name,
+                    "comment": f"\n{item.comment}" if item.comment else "",
+                }
+            )
+
         supplier_tg_id = order.supplier.telegram_id
 
         context = {
             "order": order,
             "supplier": order.supplier.get_full_name(),
             "supplier_tg_id": supplier_tg_id,
-            "time": timezone.localtime().strftime("%Y-%m-%d %H:%M"),  # 2021-08-01 12:00
+            "time": dateformat.format(timezone.localtime(), "j-F H:i"),
             "stockman": order.ordered_by.get_full_name(),
             "warehouse": order.warehouse.name,
             "order_items": order_items,
